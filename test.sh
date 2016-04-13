a77e5d9825046306a0b17a15f3beab68bc3eb0e0
gibsn <kirill-8@yandex.ru>
2016-04-13 18:01:33 +0300

    Fixed problem with unusual format photos

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ make
 mkdir -p photos
 mkdir -p thumbnails
 mkdir -p web_pages
-./Example photos.tar.gz photos thumbnails web_pages/test.html
+./Example foo.zip photos thumbnails web_pages/test.html
 open web_pages/test.html
