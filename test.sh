ab2f5684829f1d7f679a29b2182f1cd4772e0a29
Kirill Alekseev <gibsn@dione.intelib.org>
2016-11-23 02:41:33 +0300

    tm1

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 mkdir -p photos
 mkdir -p thumbnails
 mkdir -p web_pages
-make && ./Example photos.tar photos thumbnails web_pages/test.html ../photos/ ../thumbnails/ && open web_pages/test.html
+make && ./Example ../photos.tar photos thumbnails web_pages/test.html ../photos/ ../thumbnails/ && open web_pages/test.html
