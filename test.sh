473decc94a6013ea30e15bc42f49bc0e7139e7bb
Kirill Alekseev <kirill-8@yandex.ru>
2016-11-25 15:36:27 +0300

    tmp2

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p photos
-mkdir -p thumbnails
-mkdir -p web_pages
-make && ./Example ../photos.tar photos thumbnails web_pages/test.html ../photos/ ../thumbnails/ && open web_pages/test.html
